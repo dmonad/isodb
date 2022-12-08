0e339e1b807121e491bab1498bb5984d47c1d010
Kevin Jahns <kevin.jahns@protonmail.com>
2022-12-08 18:57:23 +0100

    add interfaces.js

---
+++ src/interface.js
@@ -0,0 +1,21 @@
+
+import * as error from 'lib0/error'
+import * as common from './common.js' // eslint-disable-line
+export * from './common.js'
+
+/**
+ * @template {common.IDbDef} DEF
+ *
+ * @param {string} _location
+ * @param {DEF} _def
+ * @return {Promise<common.IDB<DEF>>}
+ */
+export const openDB = async (_location, _def) => {
+  error.methodUnimplemented()
+}
+
+/**
+ * @param {string} _path
+ * @return {Promise<void>}
+ */
+export const deleteDB = (_path) => error.methodUnimplemented()
